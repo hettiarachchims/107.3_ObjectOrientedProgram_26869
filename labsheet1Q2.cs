8ba18b147293d66bb39f77f2381a374e0df61b7a
hettiarachchims <137675556+hettiarachchims@users.noreply.github.com>
2023-07-29 14:36:38 +0530

    Add files via upload

---
+++ labsheet1Q2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labsheet1Q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius, area;
+            Console.Write("Enter the radius of the circle: ");
+            radius = double.Parse(Console.ReadLine());
+            area = Math.PI * radius * radius;
+            Console.WriteLine("The area of the circle is: " + area);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ labsheet1Q3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labsheet1Q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2, sum;
+            Console.Write("Enter the first number: ");
+            num1 = double.Parse(Console.ReadLine());
+            Console.Write("Enter the second number: ");
+            num2 = double.Parse(Console.ReadLine());
+            sum = num1 + num2;
+            Console.WriteLine("The sum is: " + sum);
+            Console.ReadLine();
+        }
+    }
+}
